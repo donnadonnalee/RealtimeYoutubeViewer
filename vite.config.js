a119e46edc4b6989f7666b30c86e8d564f236c08
ds soft <ds.soft.1176106@gmail.com>
2026-03-01 09:52:07 +0900

    edited deploy settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/realtimeyoutubeviewer/', // Set base path for standalone GitHub Pages deployment
+    base: '/RealtimeYoutubeViewer/', // Set base path for standalone GitHub Pages deployment
     server: {
         host: true // Enable LAN access for testing
     }
